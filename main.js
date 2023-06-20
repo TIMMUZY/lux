f743652f68e847d42050ddef096a46de65f1a40e
TIMMUZY <tynchtykergesh02@gmail.com>
2023-06-20 20:09:04 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+let num = 10;
+let str = 'Hello world!';
+let bool = true;
+let nul = null;
+let undef = undefined;
+let obj = {};
+
+//symbol
+//bigInt
+
+//if (num > 5) {
+  //  console.log('Hello world!');
+//} else if(num === 5){
+   // console.log('Hello Geeks!');
+ //}else{
+   // console.log('bye!');
+ //}
+
+ if (num % 2 === 0) {
+    console.log('число четное');
+} 
+ }else{
+    console.log('число не четное');
+ }
+
+ alert
+
